a26599e0d81cc08a2a22df10b8cbd441221cde68
Sharma, Rajeev <Rajeev.Sharma2@mastercard.com>
2018-02-17 13:39:26 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ hi: 'rajeev' });
 });
 
-const PORT = process.envP.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
